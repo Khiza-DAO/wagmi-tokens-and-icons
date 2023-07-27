37f0924e487319f0795899da79e2910cd08aff0d
joaofranciscoguarda <joaofrancisco.guarda@gmail.com>
2023-07-27 18:50:32 -0300

    feature: dynamic type for contract, ERC20, ERC721 and custom

---
+++ src/models/ContractDict.ts
@@ -0,0 +1,34 @@
+import { Address } from "viem";
+
+type chain = {
+  address: Address;
+  id?: string;
+  name?: string;
+  symbol?: string;
+};
+
+export type ContractDict = {
+  id: string; // identificador
+  name: string; // nome legível
+  chains: {
+    [key: number]: chain;
+    // por padrão pega os dados acima, mas se passar aqui sobrescreve
+  };
+} & (
+  | ({
+      symbol: string; // símbolo legível (somente para tokens e NFTs)
+    } & (
+      | {
+          type: "ERC20"; // ERC20 (tokens), ERC721 (NFTs) ou custom
+          icon: string;
+        }
+      | {
+          type: "ERC721"; // ERC20 (tokens), ERC721 (NFTs) ou custom
+          png: string;
+        }
+    ))
+  | {
+      type: "custom"; // ERC20 (tokens), ERC721 (NFTs) ou custom
+      abi: readonly object[]; // apenas custom
+    }
+);
