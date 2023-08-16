1a50aa570ee859edb9246b3c2c620e3b0eb1de3c
joaofranciscoguarda <joaofrancisco.guarda@gmail.com>
2023-08-16 16:59:15 -0300

    chore: changed and added different export ways

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-export * from './networks'
+export type {
+  KhizaContractDict,
+  ContractDict,
+  ContractDictConfig,
+  ContractType,
+  ChainKey,
+  TokenContract,
+  CustomContract,
+  KnownContract,
+  KnownContractReturn,
+  KnownContracts,
+  NestedKnownContract,
+  KnownContractKey,
+  RootKnownContract,
+  StopRecursion,
+} from "./models";
+
+export type { ChainDict } from "./networks";
+
+export {
+  arbitrum,
+  avalanche,
+  avalancheFuji,
+  bsc,
+  bscTestnet,
+  fantom,
+  gnosis,
+  goerli,
+  mainnet,
+  optimismGoerli,
+  polygon,
+  polygonMumbai,
+  sepolia,
+} from "./networks";
---
+++ src/models/index.ts
@@ -0,0 +1,3 @@
+export type * from "./ContractDict";
+export * from "./Dict";
+
