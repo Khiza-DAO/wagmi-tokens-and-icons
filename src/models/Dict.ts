6bd7b7777b935227954820378e102fa3b4239209
joaofranciscoguarda <joaofrancisco.guarda@gmail.com>
2023-08-16 16:56:04 -0300

    chore: added js docs for dict functions

--- src/models/Dict.ts
+++ src/models/Dict.ts
@@ -10,6 +10,9 @@ import {
 } from "./ContractDict";
 import { erc20ABI, erc721ABI } from "../assets/ERCabi";
 
+/**
+ * Dictionary of known contracts with their associated information.
+ */
 export class KhizaContractDict {
   private knownContracts: KnownContracts = {};
   private selectedChain: ChainKey = 1;
@@ -19,24 +22,47 @@ export class KhizaContractDict {
     custom: [],
   };
 
+  /**
+   * Constructs a new instance of the `KhizaContractDict` class.
+   * @param config - An optional array of contract configurations to initialize the dictionary with.
+   */
   constructor(config: ContractDictConfig) {
-    for (const contract of config.knownContracts || []) {
+    config?.forEach((contract) => {
       this.addContract(contract.id, contract);
-    }
+    });
   }
 
+  /**
+   * Adds a contract to the dictionary.
+   * @param id - The identifier of the contract.
+   * @param contract - The contract information.
+   */
   addContract(id: string, contract: ContractDict) {
     this.knownContracts[id] = contract;
   }
+
+  /**
+   * Sets the selected chain.
+   * @param chain - The chain key.
+   */
 
   setChain(chain: ChainKey) {
     this.selectedChain = chain;
   }
 
-  getContract(id: KnownContractKey): KnownContractReturn | null {
+  /**
+   * Retrieves a contract from the dictionary.
+   * @param id - The identifier of the contract.
+   * @returns The contract information if found, or `undefined` otherwise.
+   * @throws An error if no contracts were added or if ABI is required for custom contracts but not provided.
+   */
+  getContract(id: KnownContractKey): KnownContractReturn | undefined {
+    if (Object.keys(this.knownContracts).length === 0)
+      throw new Error("No contracts were added");
+
     const rootContract = this.knownContracts[id];
     if (!rootContract) {
-      return null;
+      return undefined;
     }
 
     const chain = rootContract.chains[this.selectedChain] || {};
